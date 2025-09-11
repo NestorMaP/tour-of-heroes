6e625b69224061b9d7d18c126b77e493cf77170d
Nemateo <nestor.mateo2@soprasteria.com>
2025-09-11 13:52:46 +0200

    Feat: HTML and CSS for dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+    {
         path: 'heroes',
         component: HeroesComponent
-    }
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
 ];
