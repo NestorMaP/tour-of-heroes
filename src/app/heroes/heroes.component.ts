d680c7088a168134bb133cc08593a9adf29a5d93
Nemateo <nestor.mateo2@soprasteria.com>
2025-09-11 09:24:21 +0200

    Feat: Make heroes component standalone

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
+  standalone: true,
   imports: [],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
