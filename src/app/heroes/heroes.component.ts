dcfc8f736206769db969ee50b8bae2db704437e3
Nemateo <nestor.mateo2@soprasteria.com>
2025-09-11 12:33:58 +0200

    Feat: New message record when selecting a hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -21,20 +22,23 @@ export class HeroesComponent {
   selectedHero?: Hero;
 
   constructor(
-    private heroService: HeroService
+    private heroService: HeroService,
+    private messageService: MessageService
   ) {}
 
   ngOnInit() {
     this.getHeroes()
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero with id=${hero.id}.`);
+    
+  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
 }
