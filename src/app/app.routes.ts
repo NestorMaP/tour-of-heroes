5a786a68e356ca1364b147628d324ad2cfd7656a
Nemateo <nestor.mateo2@soprasteria.com>
2025-09-11 13:07:32 +0200

    Add heroes route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'heroes',
+        component: HeroesComponent
+    }
+];
