da4cf50841b644f95e7ed747601d805bba4edebf
Nemateo <nestor.mateo2@soprasteria.com>
2025-09-15 08:39:41 +0200

    Feat app.config with mock server

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data/in-memory-data.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-  ]
+    ...(isDevMode())
+      ? [
+        importProvidersFrom(
+          HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+            dataEncapsulation: false,
+            delay: 300,
+            passThruUnknownUrl: true
+          })
+        )
+      ]
+    : []
+  ],
 };
